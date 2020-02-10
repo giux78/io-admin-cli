e9fe38d254fa6681abd5b9f5477536a9fdd040a0
Alessandro Ercolani <ale.ercolani@gmail.com>
2020-02-10 21:12:24 +0100

    Minor

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 // TODO: write you code here
 
-process.stdout.write("Hello world");
+import * as readline from "readline";
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Quanti anni hai?  ", answer => {
+  switch (answer.toLowerCase()) {
+    case "4":
+      console.log("Ciao Saverio");
+      break;
+    case "41":
+      console.log("Ciao Ale");
+      break;
+    default:
+      console.log("Ciao Samuele");
+  }
+  rl.close();
+});
